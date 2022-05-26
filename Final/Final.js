61a0f444374f47a88806b32eb2c944d08ecc2cd8
Adrian0107 <adrianbriseno2010@gmail.com>
2022-05-26 18:39:56 -0500

    UPDATED

--- Final/Final.js
+++ Final/Final.js
@@ -1,70 +1,32 @@
-
 $(document).ready(function() {
-      $('#BotonSweet').click(function() {
-                swal({
-                      title: "Registroooooo exitoso",
-                      text: "Datos cargados correctamente!!",
-                      icon: "success",
-                  });
-      });
+
       $('#btnConsultar').click(function() {
-      swal({
-            title: "Registro exitoso",
-            text: "Datos cargados correctamente!!",
-            icon: "success",
-        });
-});
-});
-
-$('#btnjson').click(function() {
-    $.post('BotonJson.php',{},function(data){
-
-        console.log(data);
-        $('#idFecha').val(data.idFecha);
-        $('#idHora').val(data.idHora);
-        $('#nomPaciente').val(data.nomPaciente);
-        $('#patPaciente').val(data.patPaciente);
-        $('#matPaciente').val(data.matPaciente);
-        $('#dirCliente').val(data.dirCliente);
-        $('#colCliente').val(data.colCliente);
-        $('#codCliente').val(data.codCliente);
-        $('#emailPaciente').val(data.emailPaciente);
-        $('#telPaciente').val(data.telPaciente);
-        $('#ciudad').val(data.ciudad);
-        $('#estado').val(data.estado);
-
-    },'json');
-});
-
-$('#btnFetch').click(function(){
-      fetch('PracticaFetch.php')
-            .then((respuesta) =>  respuesta.json() )
-                  .then(function(dato) { refrescar(dato) });
-});      
+            let varid =prompt('Ingrese un ID para consultar');
+            $.post('./php/Consultar.php',{par1:varid},function(data){
+            refrescar(data);
+            },'json');
+      });
+      
       function refrescar(objeto){
-            $('#idFecha').val(objeto.idFecha);
-            $('#idHora').val(objeto.idHora);
-            $('#nomPaciente').val(objeto.nomPaciente);
-            $('#patPaciente').val(objeto.patPaciente);
-            $('#matPaciente').val(objeto.matPaciente);
-            $('#dirCliente').val(objeto.dirCliente);
-            $('#colCliente').val(objeto.colCliente);
-            $('#codCliente').val(objeto.codCliente);
-            $('#emailPaciente').val(objeto.emailPaciente);
-            $('#telPaciente').val(objeto.telPaciente);
-            $('#ciudad').val(objeto.ciudad);
-            $('#estado').val(objeto.estado);      
+          console.log(objeto);
+          $('#idFecha').val(objeto.idFecha);
+          $('#idHora').val(objeto.idHora);
+          $('#nomPaciente').val(objeto.nomPaciente);
+          $('#patPaciente').val(objeto.patPaciente);
+          $('#matPaciente').val(objeto.matPaciente);
+          $('#dirCliente').val(objeto.dirCliente);
+          $('#colCliente').val(objeto.colCliente);
+          $('#codCliente').val(objeto.codCliente);
+          $('#emailPaciente').val(objeto.emailPaciente);
+          $('#telPaciente').val(objeto.telPaciente);
+          $('#ciudad').val(objeto.ciudad);
+          $('#estado').val(objeto.estado);  
       }
 
-$('#btnConsulta').click(function(){
-      $('modal1').modal1();
-      $('modal1').on('hiden.bs.modal', function (e){
-            let varid=$('#idconsulta').val();
-            $.post('./pho/getRegistroDB.php',{par1:varid},function(){
-                  refrescar(data);
-            }, 'json');
-      })
+      
 });
+
+
 
 
 
--- Final/php/Consultar.php
+++ Final/php/Consultar.php
@@ -1,13 +1,12 @@
 <?php
-$paridCte=$POST['par1'];
+$paridCte=$_POST['par1'];
 $hostname='localhost';
 $database= 'L19100152';
 $username= 'root';
 $password='';
-$port='3306';
 
 try{
-    $con = new PDO("mysql:host=$hostname;dbname=$database;port=$port",$username,$password);
+    $con = new PDO("mysql:host=$hostname; dbname=$database" ,$username,$password);
 } catch (PDOExcepction $e){
     echo "Error en la conexion a la base de datos";
     echo $e ->getMessage();
@@ -16,7 +15,7 @@ try{
 
 //$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 try{
-    $consultaSql="select * from Cita where ID=".$paridCte;
+    $consultaSql = "select * from cita where ID =".$paridCte;
     $consulta = $con -> prepare($consultaSql);
     $consulta -> execute();
     $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
